75c26a74adf5b81ab4aabda698afb1b4f962be19
Seokwon Kang <kangsw1988@gmail.com>
2025-05-13 11:53:55 +0900

    feat: React 19 이상에서의 props 기본값 설정 방식 변경 및 이벤트 핸들링 추가

--- section05/section05.md
+++ section05/section05.md
@@ -49,3 +49,44 @@ const Main = () => {
   );
 };
 ```
+
+## React 버전 별 Props 기본값 설정
+
+### React 18 까지
+
+React 18까지는 컴포넌트에 `defaultProps` 객체를 사용하여 props의 기본값을 설정할 수 있었다.
+
+```javascript
+Button.defaultProps = {
+  text: "button",
+  color: "black",
+};
+```
+
+### React 19 이상
+
+React 19 이상에서는 defaultProps 객체가 더 이상 지원되지 않는다.  
+**구조분해 할당** 방식으로 기본값을 직접 설정해야 한다.
+
+```javascript
+const Button = ({ text = "button", color = "black" }) => {
+  ...
+}
+```
+
+## 이벤트 핸들링 (Event Handeling)
+
+웹에서 이벤트가 발생했을 때 그 것을 처리하는 것.
+
+- (ex) 버튼 클릭 시 경고창 노출
+- 이벤트(Event)
+  - 웹 내부에서 발생하는 사용자의 행동.
+  - (ex) 버튼 클릭, 메시지 입력, 스크롤 등등.
+
+## 합성 이벤트 (Synthetic Base Event)
+
+React에서 사용하는 **이벤트 객체**로, 브라우저의 네이티브 이벤트를 감싸는 크로스 브라우저 래퍼다.
+
+- Cross Browsing Issue 해결: 브라우저 간 일관된 이벤트 처리를 보장.
+- 이벤트 풀링을 통해 성능을 최적화한다.
+- 네이티브 이벤트와 유사한 인터페이스를 제공하지만 React의 방식으로 동작한다.
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,8 +1,17 @@
-const Button = ({ children, text = "button", color = "black" }) => {
+const Button = ({ text, color, children }) => {
+  const onClickButton = (e) => {
+    console.log(text);
+    console.log(e);
+  };
+
   return (
     <>
-      <button style={{ color: color }}>
-        {text} - {color.toUpperCase()}
+      <button
+        onClick={onClickButton}
+        // onMouseEnter={onClickButton}
+        style={{ color: color }}
+      >
+        {text} - {color}
         {children}
       </button>
       <br />
@@ -10,13 +19,13 @@ const Button = ({ children, text = "button", color = "black" }) => {
   );
 };
 
-// 기본 값 설정 (React 19 이상)
-//const Button = ({ text = "button", color = "black" }) => {}
+// 기본 값 설정 (React 18 까지)
+Button.defaultProps = {
+  text: "button",
+  color: "black",
+};
 
-// 기본 값 설정 (React 18 까지): 구조분해 할당 방식으로 기본값 설정하도록 변경됨.
-// Button.defaultProps = {
-//   text: "button",
-//   color: "black",
-// };
+// 기본 값 설정 (React 19 이상): 구조분해 할당 방식으로 기본값 설정하도록 변경됨.
+//const Button = ({ text = "button", color = "black" }) => {
 
 export default Button;
